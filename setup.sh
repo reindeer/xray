988086ca3afe3ff8dab23625a0f39ce13cee7fdc
Denis Daschenko <reindeer@tarandro.ru>
2023-11-23 00:09:30 +0300

    fix vars substitute

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ source .env && [ ! -z $ID ] && echo "already configured" && exit 1
 
 touch config.json
 docker-compose build
-docker-compose run xray xray uuid | while read uuid; do echo "ID=$uuid" >> .env; done
-docker-compose run xray xray x25519 | while read line; do echo $(echo $line | awk -F': ' '{print $1}' | tr 'a-z ' 'A-Z_')=$(echo $line | awk -F': ' '{print $2}'); done >> .env
-echo SHORTID=$(openssl rand -hex 8)
-source .env
+docker-compose run xray xray uuid | while read uuid; do echo "ID=$uuid" | tr -d '\r' >> .env; done
+docker-compose run xray xray x25519 | while read line; do echo $(echo $line | awk -F': ' '{print $1}' | tr 'a-z ' 'A-Z_')=$(echo $line | awk -F': ' '{print $2}') | tr -d '\r'; done >> .env
+echo SHORTID=$(openssl rand -hex 8) | tr -d '\r' >> .env
+
+for e in $(cat .env); do $(echo export $e); done
 envsubst < config.json.tpl > config.json
