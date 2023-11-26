3fdecf82f8653f4a41d4fba69fe6e7334967ab88
Denis Daschenko <reindeer@tarandro.ru>
2023-11-26 10:42:35 +0300

    add some bash magick

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ source .env && [ ! -z $ID ] && echo "already configured" && exit 1
 touch config.json
 docker-compose build
 docker-compose run --rm xray xray uuid | while read uuid; do echo "ID=$uuid" | tr -d '\r' >> .env; done
-docker-compose run --rm xray xray x25519 | while read line; do echo $(echo $line | awk -F': ' '{print $1}' | tr 'a-z ' 'A-Z_')=$(echo $line | awk -F': ' '{print $2}') | tr -d '\r'; done >> .env
+docker-compose run --rm xray xray x25519 | while read line; do echo ${line/ /_} | tr -d '\r' | awk -F': ' '{print toupper($1)"="$2}'; done >> .env
 echo SHORTID=$(openssl rand -hex 8) | tr -d '\r' >> .env
 
 for e in $(cat .env); do $(echo export $e); done
